85f4041fa5913dd1c301c5da8dcf5a7d00a4f538
louip22 <louip22@student.sdu.dk>
2024-02-15 17:16:06 +0100

    Player bullet fires with delay
    ready method implemented in playercontrolsystem to create delay on bullet fiering

--- Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/Player.java
+++ Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/Player.java
@@ -2,10 +2,21 @@ package dk.sdu.mmmi.cbse.playersystem;
 
 import dk.sdu.mmmi.cbse.common.data.Entity;
 
+import java.security.Timestamp;
+
 /**
  *
  * @author Emil
  */
 public class Player extends Entity {
 
+    private long bulletFiredTime;
+
+    public long getBulletFiredTime() {
+        return bulletFiredTime;
+    }
+
+    public void setBulletFiredTime(long bulletFiredTime) {
+        this.bulletFiredTime = bulletFiredTime;
+    }
 }
