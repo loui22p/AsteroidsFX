4278e14563db1da642c23f7776fdafb555e3b095
louip22 <louip22@student.sdu.dk>
2024-04-12 08:29:02 +0200

    split packages made
    split packages made and gives error

--- AsteroidTest/src/main/java/module-info.java
+++ AsteroidTest/src/main/java/module-info.java
@@ -1,14 +1,14 @@
+import dk.sdu.mmmi.cbse.SplitterPackage;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
-import dk.sdu.mmmi.cbse.asteroidsystem.AsteroidControlSystem;
-import dk.sdu.mmmi.cbse.asteroidsystem.AsteroidPlugin;
 
 module AstroidTest {
     exports dk.sdu.mmmi.cbse.asteroidsystem;
+    exports dk.sdu.mmmi.cbse;
     requires Common;
     requires CommonBullet;
     uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
-    provides IGamePluginService with AsteroidPlugin;
-    provides IEntityProcessingService with AsteroidControlSystem;
+    provides IGamePluginService with dk.sdu.mmmi.cbse.asteroidsystem.AsteroidPlugin;
+    provides IEntityProcessingService with dk.sdu.mmmi.cbse.asteroidsystem.AsteroidControlSystem, SplitterPackage;
 
 }
