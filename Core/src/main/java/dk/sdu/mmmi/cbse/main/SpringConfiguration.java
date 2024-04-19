71239522eb06cf7d7aef1bee930efe10c4d1ddf0
louip22 <louip22@student.sdu.dk>
2024-04-19 17:18:29 +0200

    Spring dependencies in maven
    dependencies included in pom.xml files in core and in root

---
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/SpringConfiguration.java
@@ -0,0 +1,35 @@
+package dk.sdu.mmmi.cbse.main;
+
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+
+import java.beans.BeanProperty;
+import java.util.List;
+import java.util.ServiceLoader;
+import static java.util.stream.Collectors.toList;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfiguration {
+
+    @Bean
+    public SpringConfiguration() {
+    }
+
+    @Bean
+    public List<IEntityProcessingService> entityProcessingServiceList(){
+        return ServiceLoader.load(IEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+
+    @Bean
+    public List<IGamePluginService> gamePluginServices() {
+        return ServiceLoader.load(IGamePluginService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+
+    @Bean
+    public List<IPostEntityProcessingService> postEntityProcessingServices() {
+        return ServiceLoader.load(IPostEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+}
